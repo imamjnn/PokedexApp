83bc81d286b01e3bdc2acdc93b867fc9da1ea46b
Imam Jinani <immjnn@gmail.com>
2023-03-14 11:47:31 +0700

    setup prettier and path alias

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,23 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react', 'prettier', 'react-hooks'],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        '@typescript-eslint/no-shadow': ['error'],
+        'no-shadow': 'off',
+        'no-undef': 'off',
+
+        // eslint-plugin-prettier
+        'prettier/prettier': 2,
+
+        // react-hooks
+        'react-hooks/rules-of-hooks': 'error',
+        'react-hooks/exhaustive-deps': 'warn',
+      },
+    },
+  ],
 };
--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,22 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        alias: {
+          '@screens': './src/screens',
+          '@components': './src/components',
+          '@navigation': './src/navigation',
+          '@utils': './src/utils',
+          '@themes': './src/themes',
+          '@services': './src/services',
+          '@hooks': './src/hooks',
+          '@assets': './assets',
+        },
+        extensions: ['.ios.js', '.android.js', '.js', '.jsx', '.json', '.tsx', '.ts', '.native.js'],
+      },
+    ],
+  ],
 };
